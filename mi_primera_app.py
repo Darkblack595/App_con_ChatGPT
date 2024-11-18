8c2e52218f0bce325cb42f76d9c787b57f8401be
Juan Pablo Gaviria Orozco <141966367+Darkblack595@users.noreply.github.com>
2024-11-18 15:50:01 -0500

    Update mi_primera_app.py

--- mi_primera_app.py
+++ mi_primera_app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 
-# Título de la aplicación
+# Título
 st.title("Mi primera app")
 
-# Autor de la aplicación
-st.write("Esta app fue elaborada por Juan Pablo Gaviria Orozco")
+# Autor
+st.write("Esta app fue elaborada por “Juan Pablo Gaviria Orozco”.")
+
+# Entrada de usuario
+nombre_usuario = st.text_input("Por favor, escribe tu nombre:")
+
+# Saludo personalizado
+if nombre_usuario:
+    st.write(f"{nombre_usuario}, te doy la bienvenida a mi primera app.")
